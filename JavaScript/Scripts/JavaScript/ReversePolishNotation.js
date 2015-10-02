bb36ae6159e0743e6a3852e2e9c9f26dfeb73840
Eric Helin <ehelin@felpsis.net>
2015-10-01 22:18:10 -0500

    Completed reverse polish notation example

--- JavaScript/Scripts/JavaScript/ReversePolishNotation.js
+++ JavaScript/Scripts/JavaScript/ReversePolishNotation.js
@@ -1,46 +1,60 @@
-﻿
+﻿var Stack = [];
+
 function RunPolishReverseNotation() {
-    //RunSingleOperatorTests();
-    RunMultipleOperatorTests();
+    RunTests();
 }
+function RunTests() {
+    var asserts = new Asserts();
 
-//Multiple operator/stack using out of the box Javascript array =========================================
-var MultipleStack = [];
-
-function RunMultipleOperatorTests() {
-    var result = RunMultipleOperatorTest('5 6 * 3 2 *', false);
-    result = RunMultipleOperatorTest('5 6 * 3 2 * 3 * -', true);
+    asserts.areEqual(RunTest('5 6 * 3 2 * 3 * -', false), 12);
+    asserts.areEqual(RunTest('5 6 * 3 2 * 3 * -', true), 12);
+    asserts.areEqual(RunTest('2 4 *', false), 8);
+    asserts.areEqual(RunTest('2 4 *', true), 8);
+    asserts.areEqual(RunTest('10 3 /', false), 3);
+    asserts.areEqual(RunTest('10 3 /', true), 3);
+    asserts.areEqual(RunTest('7 3 +', false), 10);
+    asserts.areEqual(RunTest('7 3 +', true), 10);
+    asserts.areEqual(RunTest('45 3 -', false), 42);
+    asserts.areEqual(RunTest('45 3 -', true), 42);
+    asserts.areEqual(RunTest('4 3 %', false), 1);
+    asserts.areEqual(RunTest('4 3 %', true), 1);
+    asserts.areEqual(RunTest('17 4 &', false), 0);
+    asserts.areEqual(RunTest('17 4 &', true), 0);
+    asserts.areEqual(RunTest('511 56 ^', false), 455);
+    asserts.areEqual(RunTest('511 56 ^', true), 455);
+    asserts.areEqual(RunTest('42 21 |', false), 63);
+    asserts.areEqual(RunTest('42 21 |', true), 63);
 }
-
-function RunMultipleOperatorTest(expression, useEval) {
-    PopulateMultipleStack(expression, useEval);
+function RunTest(expression, useEval) {
+    var expressionArr = expression.split(' ');
     var cont = true;
+    var op1 = null, op2 = null, operator = null, result = 0;
 
-    while (cont) 
+    for(var i=0;i< expressionArr.length;i++)
     {
-        var op1 = null,op2 = null, operator = null, result = 0;
+        var value = expressionArr[i];
 
-        while (operator == null || op1 == null || op2 == null)
+        if (!isNaN(value))
         {
-            var val = MultipleStack.pop();
-            
-            if (isNaN(val))
-                operator = val;
-            else if (op1 == null)
-                op1 = val;
-            else if (op2 == null)
-                op2 = val;
+            var value = parseInt(value);
+            Stack.push(value);
+        }
+        else
+        {
+            operator = value;
+            op2 = Stack.pop();
+            op1 = Stack.pop();
         }
 
-        result = GetValue(op1, op2, operator, useEval);
-
-        result = parseInt(result);
-
-        ctr++;
+        if (operator != null && op1 != null && op2 != null)
+        {
+            result = GetValue(op1, op2, operator, useEval);
+            result = parseInt(result);                
+            Stack.push(result);
+            op1 = null, op2 = null, operator = null, value = null;
+        }            
     }
-
-    Stack.push(result);
-
+    
     var calculatedValue = Stack.pop();
 
     return calculatedValue;
@@ -69,126 +83,3 @@ function GetValue(op1, op2, operator, useEval) {
 
     return result;
 }
-function PopulateMultipleStack(expression, useEval) {
-    var expressionArr = expression.split(' ');
-
-    for (var i = 0; i < expressionArr.length; i++) {
-        var value = expressionArr[i];
-
-        if (!useEval && !isNaN(value))
-            value = parseInt(expressionArr[i]);
-
-        MultipleStack.push(value);
-    }
-}
-
-
-//Single operator/stack using out of the box Javascript array =========================================
-var Stack = [];
-
-function RunSingleOperatorTests() {
-    var asserts = new Asserts();
-
-    asserts.areEqual(RunSingleOperatorTest('2 4 *', false), 8);
-    asserts.areEqual(RunSingleOperatorTest('2 4 *', true), 8);
-    asserts.areEqual(RunSingleOperatorTest('10 3 /', false), 3);
-    asserts.areEqual(RunSingleOperatorTest('10 3 /', true), 3);
-    asserts.areEqual(RunSingleOperatorTest('7 3 +', false), 10);
-    asserts.areEqual(RunSingleOperatorTest('7 3 +', true), 10);
-    asserts.areEqual(RunSingleOperatorTest('45 3 -', false), 42);
-    asserts.areEqual(RunSingleOperatorTest('45 3 -', true), 42);
-    asserts.areEqual(RunSingleOperatorTest('4 3 %', false), 1);
-    asserts.areEqual(RunSingleOperatorTest('4 3 %', true), 1);
-    asserts.areEqual(RunSingleOperatorTest('17 4 &', false), 0);
-    asserts.areEqual(RunSingleOperatorTest('17 4 &', true), 0);
-    asserts.areEqual(RunSingleOperatorTest('511 56 ^', false), 455);
-    asserts.areEqual(RunSingleOperatorTest('511 56 ^', true), 455);
-    asserts.areEqual(RunSingleOperatorTest('42 21 |', false), 63);
-    asserts.areEqual(RunSingleOperatorTest('42 21 |', true), 63);
-
-}
-
-function RunSingleOperatorTest(expression, useEval) {
-    var result = 0;
-
-    PopulateStack(expression, useEval);
-
-    var operator = Stack.pop();
-    var op2 = Stack.pop();
-    var op1 = Stack.pop();
-
-    result = GetValue(op1, op2, operator, useEval);
-
-    result = parseInt(result);
-
-    Stack.push(result);
-
-    var calculatedValue = Stack.pop();
-
-    return calculatedValue;
-}
-function GetValue(op1, op2, operator, useEval)
-{
-    if (useEval != null && useEval != undefined && useEval == true)
-        result = eval(op1 + operator + op2);
-    else if (operator == "*")
-        result = op1 * op2;
-    else if (operator == "+")
-        result = op1 + op2;
-    else if (operator == "-")
-        result = op1 - op2;
-    else if (operator == "/")
-        result = op1 / op2;
-    else if (operator == "%")
-        result = op1 % op2;
-    else if (operator == "&")
-        result = op1 & op2;
-    else if (operator == "^")
-        result = op1 ^ op2;
-    else if (operator == "|")
-        result = op1 | op2;
-    else 
-        alert('unknown operator');
-
-    return result;
-}
-function PopulateStack(expression, useEval) {
-    var expressionArr = expression.split(' ');
-
-    for (var i = 0; i < expressionArr.length; i++) {
-        var value = expressionArr[i];
-
-        if (!useEval && !isNaN(value))
-            value = parseInt(expressionArr[i]);
-
-        Stack.push(value);
-    }
-}
-
-
-function InsertValue(insertValue, endIndex) {
-    var ctr = 0;
-    var newStack = [];
-
-    newStack[0] = insertValue;
-
-    for (var i = 0; i < Stack.length; i++) {
-        newStack[ctr + 1] = Stack[ctr];
-    }
-
-    Stack = newStack;
-}
-function GetEndIndex() {
-    var endIndex = 0;
-
-    for (var i = 0; i < Stack.length; i++) {
-        if (Stack[i] == null) {
-            endIndex = i;
-            break;
-        }
-    }
-
-    return endIndex;
-}
-
-
